b983089e763d822b7f814fabb4313e569bc4fc9b
simodewit <117769294+simodewit@users.noreply.github.com>
2024-04-18 02:50:02 +0200

    updated car movement
    i have introduced the ability of going in reverse, being in neutral, and going thru different gears for different top speeds. i have also refined the braking and steering a little bit

--- Racer/Assets/Scripts/Car.cs
+++ Racer/Assets/Scripts/Car.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -9,13 +10,6 @@ public enum PowerDeliviry
     AWD,
     RWD,
     FWD
-}
-
-public enum GearType
-{
-    Drive,
-    Neutral,
-    Reverse
 }
 
 public class Car : MonoBehaviour
@@ -37,18 +31,24 @@ public class Car : MonoBehaviour
     [SerializeField] private PowerDeliviry powerDeliviry = PowerDeliviry.AWD;
     [Tooltip("The total newton meters of torque that the car has"), Range(0, 5000)]
     [SerializeField] private float engineTorque = 200;
+    [Tooltip("The amount of torque that is used of the engineTorque when in reverse"), Range(0, 1)]
+    [SerializeField] private float reverseTorque;
+    [Tooltip("The max speed that the car is capable of hitting")]
+    [SerializeField] private float topSpeed = 180;
+    //[Tooltip("The highest amount of rpm for of the car"), Range(0,20000)]
+    //[SerializeField] private float maxRPM = 5000;
+    [Tooltip("The torque given at specific moments")]
+    [SerializeField] private AnimationCurve torqueCurve;
     [Tooltip("The info for every gear")]
     [SerializeField] private GearInfo[] gearInfo;
-    [Tooltip("The lowest amount of rpm for of the car")]
-    [SerializeField] private float minRPM;
-    [Tooltip("The highest amount of rpm for of the car")]
-    [SerializeField] private float maxRPM;
 
     [Header("Brakes")]
     [Tooltip("The total newton meters of torque that the car has"), Range(0, 5000)]
     [SerializeField] private float brakeTorque = 200;
     [Tooltip("The amount of brakes to the front or back of the car"), Range(0, 100)]
     [SerializeField] private float brakeBalance = 55;
+    [Tooltip("The deadzone in the pedal before the throttle or brake is used")]
+    [SerializeField] private float pedalDeadzone;
 
     [Header("Steering")]
     [Tooltip("The amount of speed at witch you steer the car"), Range(0, 3)]
@@ -60,20 +60,38 @@ public class Car : MonoBehaviour
     [Tooltip("The speed at witch the steering snaps back towards the middle"), Range(0, 0.1f)]
     [SerializeField] private float snapBackSpeed = 0.02f;
 
-    private float steeringAmount;
+    private Rigidbody rb;
     private int currentGear;
+    private float steeringAmount;
+    private float backwardTorque;
+    private float speedInKph;
 
     #endregion
 
-    #region update
+    #region start and update
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        backwardTorque = -engineTorque * reverseTorque;
+    }
 
     public void Update()
     {
         Turning();
+        Speedometer();
+        GearShifts();
         AllWheelDrive();
-        RearWheelDrive();
-        FrontWheelDrive();
         Braking();
+    }
+
+    #endregion
+
+    #region speedometer
+
+    public void Speedometer()
+    {
+        speedInKph = rb.velocity.magnitude * 3.6f;
     }
 
     #endregion
@@ -104,6 +122,23 @@ public class Car : MonoBehaviour
 
     #endregion
 
+    #region gear shifts
+
+    public void GearShifts()
+    {
+        //temporary shifting code. should be changed to the new input system soon
+        if (Input.GetKeyDown(KeyCode.E) && currentGear < gearInfo.Length - 1)
+        {
+            currentGear += 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && currentGear > 0)
+        {
+            currentGear -= 1;
+        }
+    }
+
+    #endregion
+
     #region AllWheelDrive
 
     public void AllWheelDrive()
@@ -115,87 +150,57 @@ public class Car : MonoBehaviour
 
         float axis = Input.GetAxis("Vertical");
 
-        if (axis > 0)
+        if (axis > pedalDeadzone)
         {
-            float torquePerWheel = CalculateTorque(axis) * 0.25f;
+            float wheelTorque = CalculateTotalTorque(gearInfo[currentGear]) * 0.25f;
 
-            FL.motorTorque = torquePerWheel;
-            FR.motorTorque = torquePerWheel;
-            RL.motorTorque = torquePerWheel;
-            RR.motorTorque = torquePerWheel;
+            FL.motorTorque = wheelTorque;
+            FR.motorTorque = wheelTorque;
+            RL.motorTorque = wheelTorque;
+            RR.motorTorque = wheelTorque;
+        }
+        else
+        {
+            FL.motorTorque = 0;
+            FR.motorTorque = 0;
+            RL.motorTorque = 0;
+            RR.motorTorque = 0;
         }
     }
 
     #endregion
 
-    #region RearWheelDrive
+    #region torque
 
-    public void RearWheelDrive()
+    public float CalculateTotalTorque(GearInfo gear)
     {
-        if (powerDeliviry != PowerDeliviry.RWD)
+        float totalTorque;
+
+        if (gear.gearNumber != -1)
         {
-            return;
+            float placeInCurve = (speedInKph / 2) / (topSpeed / 2);
+            float torqueMultiplier = torqueCurve.Evaluate(placeInCurve);
+
+            if (speedInKph >= gear.maxSpeed)
+            {
+                torqueMultiplier = 0;
+            }
+
+            totalTorque = engineTorque * torqueMultiplier;
+        }
+        else
+        {
+            float torque = backwardTorque;
+
+            if (speedInKph <= gear.maxSpeed)
+            {
+                torque = 0;
+            }
+
+            totalTorque = torque;
         }
 
-        float axis = Input.GetAxis("Vertical");
-
-        if (axis > 0)
-        {
-            float torquePerWheel = engineTorque * 0.5f;
-
-            RL.motorTorque = torquePerWheel * axis;
-            RR.motorTorque = torquePerWheel * axis;
-        }
-    }
-
-    #endregion
-
-    #region FrontWheelDrive
-
-    public void FrontWheelDrive()
-    {
-        if (powerDeliviry != PowerDeliviry.FWD)
-        {
-            return;
-        }
-
-        float axis = Input.GetAxis("Vertical");
-
-        if (axis > 0)
-        {
-            float torquePerWheel = engineTorque * 0.5f;
-
-            FL.motorTorque = torquePerWheel * axis;
-            FR.motorTorque = torquePerWheel * axis;
-        }
-    }
-
-    #endregion
-
-    #region gears
-
-    private float CalculateTorque(float axis)
-    {
-        // Get the gear ratio from another class with more info
-        float gearRatio = gearInfo[currentGear].gearRatio;
-
-        // Calculate engine RPM based on the front left wheel RPM
-        float engineRPM = RL.rpm * gearRatio;
-
-        print(engineRPM);
-
-        // Ensure engine RPM is within the valid range
-        float normalizedRPM = Mathf.Clamp01((engineRPM - minRPM) / (maxRPM - minRPM));
-
-        // Define torque curve points
-        float lowTorque = 0.2f * engineTorque;
-        float highTorque = engineTorque;
-
-        // Interpolate torque based on RPM
-        float torque = Mathf.Lerp(lowTorque, highTorque, Mathf.Pow(normalizedRPM, 2));
-
-        torque *= axis;
-        return torque;
+        return totalTorque;
     }
 
     #endregion
@@ -204,9 +209,10 @@ public class Car : MonoBehaviour
 
     public void Braking()
     {
+        //temporary braking code. should be changed to the new input system soon
         float axis = Input.GetAxis("Vertical");
 
-        if (axis < 0)
+        if (axis < pedalDeadzone)
         {
             float frontBrakes = brakeBalance / 100;
             float rearBrakes = (100 - brakeBalance) / 100;
@@ -233,6 +239,6 @@ public class Car : MonoBehaviour
 [Serializable]
 public class GearInfo
 {
-    public GearType gearType;
-    public float gearRatio;
+    public int gearNumber;
+    public float maxSpeed;
 }
