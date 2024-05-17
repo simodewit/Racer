96c3a1766c852515cbc40d02cb36ce88c8d7c9fc
simodewit <117769294+simodewit@users.noreply.github.com>
2024-05-17 02:53:28 +0200

    changes in the wheelcontroller script
    i have made a lot of inprovements, added gizmos for understanding the problems, and tweaked a bit

--- Racer/Assets/Scripts/Car/WheelController.cs
+++ Racer/Assets/Scripts/Car/WheelController.cs
@@ -11,21 +11,25 @@ public class WheelController : MonoBehaviour
     [SerializeField] private Rigidbody carRb;
 
     [Header("Spring")]
-    [SerializeField] private float forceOffset;
     [SerializeField] private float springTravel;
-    [Tooltip("The progression of the stiffness of the spring"), Curve(0f, 0f, 1f, 1f, true)]
+    [Tooltip("The progression of the stiffness of the spring calculated from its position in its travel"), Curve(0f, 0f, 1f, 1f, true)]
     [SerializeField] private AnimationCurve springCurve;
+    [Tooltip("The offset you can give for the place where the spring wants to go")]
     [SerializeField] private float targetPosOffset;
     [SerializeField] private float spring;
     [SerializeField] private float damper;
 
-    public bool debug;
+    public bool info;
 
+    //variables for the gizmos
     private Vector3 gizmosSpringForce;
     private Vector3 gizmosSpringApplyPos;
+    private float gizmosSpringDistance;
 
     private Rigidbody rb;
-    private Vector3 targetPos;
+    private Transform targetPos;
+
+    private bool isGrounded;
 
     #endregion
 
@@ -33,8 +37,9 @@ public class WheelController : MonoBehaviour
 
     public void Start()
     {
+        MakeTargetPos();
+
         rb = GetComponent<Rigidbody>();
-        targetPos = transform.localPosition + new Vector3(0, targetPosOffset, 0);
     }
 
     public void FixedUpdate()
@@ -45,17 +50,34 @@ public class WheelController : MonoBehaviour
 
     #endregion
 
+    #region startPosition
+
+    private void MakeTargetPos()
+    {
+        Vector3 placeToCreate = transform.localPosition + new Vector3(0, targetPosOffset, 0);
+        targetPos = new GameObject().transform;
+        targetPos.parent = transform.parent;
+        targetPos.localPosition = placeToCreate;
+        targetPos.name = "SpringTargetPos";
+    }
+
+    #endregion
+
     #region clamp
 
     private void Clamps()
     {
         Vector3 clamp = transform.localPosition;
-        Mathf.Clamp(clamp.y, -springTravel / 2, springTravel / 2);
-        clamp.x = targetPos.x;
-        clamp.z = targetPos.z;
-        transform.localPosition = clamp;
 
+        float startPos = targetPos.localPosition.y;
+        clamp.y = Mathf.Clamp(clamp.y, (-springTravel / 2) + startPos, (springTravel / 2) + startPos);
+
+        clamp.x = targetPos.localPosition.x;
+        clamp.z = targetPos.localPosition.z;
+
+        transform.localPosition = clamp;
         transform.localRotation = Quaternion.identity;
+
         Vector3 vel = rb.velocity;
         vel.x = 0;
         vel.z = 0;
@@ -68,7 +90,7 @@ public class WheelController : MonoBehaviour
 
     private void Suspension()
     {
-        float distance = targetPos.y - transform.localPosition.y;
+        float distance = targetPos.localPosition.y - transform.localPosition.y;
 
         float graphValue = Mathf.Abs(distance) / (springTravel / 2);
         float springProgression = springCurve.Evaluate(graphValue);
@@ -78,25 +100,52 @@ public class WheelController : MonoBehaviour
 
         Vector3 direction = -carRb.transform.up * force;
 
-        float yAxisOffset = targetPos.y + springTravel / 2 + forceOffset;
-        Vector3 forcePoint = new Vector3(targetPos.x, yAxisOffset, targetPos.z) + carRb.transform.position;
+        Vector3 offset = new Vector3(0, springTravel / 2, 0);
+        Vector3 forcePoint = carRb.transform.TransformPoint(targetPos.localPosition + offset);
         
         carRb.AddForceAtPosition(direction, forcePoint);
 
+        //info for the gizmos
         gizmosSpringForce = direction;
         gizmosSpringApplyPos = forcePoint;
+        gizmosSpringDistance = distance;
+
+        if (info)
+        {
+            print("Distance = " + distance + "Force = " + force);
+        }
     }
 
     #endregion
 
     #region gizmos
 
+    //i am using gizmos for properly understanding and vizualizing the results of the system
     private void OnDrawGizmos()
     {
-        Vector3 toShow = gizmosSpringForce / spring * 10 + transform.position;
-        Gizmos.DrawLine(transform.position, toShow);
-
+        //draws the place where the force is applied
+        Gizmos.color = Color.cyan;
         Gizmos.DrawCube(gizmosSpringApplyPos, new Vector3(0.1f, 0.1f, 0.1f));
+
+        //draws the lowest place of the spring
+        Gizmos.color = Color.cyan;
+        Vector3 lowestSpringPoint = new Vector3(gizmosSpringApplyPos.x, gizmosSpringApplyPos.y - springTravel, gizmosSpringApplyPos.z);
+        Gizmos.DrawCube(lowestSpringPoint, new Vector3(0.1f, 0.1f, 0.1f));
+
+        //draws the travel of the spring
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(gizmosSpringApplyPos, lowestSpringPoint);
+
+        //draws the place where the wheel is in the travel of the spring
+        Vector3 middleOfSpring = lowestSpringPoint + new Vector3(0, springTravel / 2, 0);
+        Vector3 wheelPos = middleOfSpring + new Vector3(0, gizmosSpringDistance, 0);
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(wheelPos, new Vector3(0.1f, 0.1f, 0.1f));
+
+        //draws the forces of the springs
+        Vector3 toShow = gizmosSpringForce / spring * 10 + transform.position;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, toShow);
     }
 
     #endregion
