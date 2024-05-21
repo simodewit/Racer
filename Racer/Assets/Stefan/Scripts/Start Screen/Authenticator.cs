881004532d11ab9d4928b349b29e65d4dc1c25d0
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2024-05-21 10:33:46 +0200

    initial commit

---
+++ Racer/Assets/Stefan/Scripts/Start Screen/Authenticator.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class Authenticator
+{
+    const string SAVE_FILE_NAME = "AuthData.json";
+
+    private static AuthData _data;
+
+    private static AuthData AuthenticatorData
+    {
+        get
+        {
+            if(_data == null )
+            {
+                string path = Path.Combine (Application.persistentDataPath, SAVE_FILE_NAME);
+
+                if ( File.Exists (path) )
+                {
+                    string json = File.ReadAllText (path);
+
+                    _data = JsonUtility.FromJson<AuthData> (json);
+                }
+                else
+                {
+                    _data = new AuthData
+                    {
+                        name = null,
+                        hasSignedIn = false,
+                    };
+                }
+            }
+            return _data;
+        }
+    }
+
+    /// <summary>
+    /// Has the user created an name?
+    /// </summary>
+    public static bool IsSignedIn
+    {
+        get
+        {
+            return AuthenticatorData.hasSignedIn;
+        }
+    }
+
+    /// <summary>
+    /// The username of the player, returns null if the user hasn't picked a name yet
+    /// </summary>
+    public static string UserName
+    {
+        get
+        {
+            return AuthenticatorData.name;
+        }
+    }
+
+    /// <summary>
+    /// Creates a user and saves the name
+    /// </summary>
+    /// <param name="name"></param>
+    public static void CreateUser(string name )
+    {
+        AuthenticatorData.name = name;
+        AuthenticatorData.hasSignedIn = true;
+    }
+
+    private class AuthData
+    {
+        public string name;
+
+        public bool hasSignedIn;
+    }
+}
